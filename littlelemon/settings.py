05c9fcfabe53699bd5cb5549ed4254c247f7eaac
ahmadrajab <ahmad.rajab221@gmail.com>
2023-07-05 20:11:32 +0300

    added apis

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'resturant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- resturant/urls.py
+++ resturant/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, menuview,bookingview, SingleMenuItemView, MenuItemsView, BookingViewSet
 urlpatterns = [
     path('', index, name='index'),
+    path('menue/', menuview.as_view()),
+    path('booking/', bookingview.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('menu/items', MenuItemsView.as_view()),
+
+
+
 ]
